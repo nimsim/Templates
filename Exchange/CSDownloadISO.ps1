e7d473b37a5b5d7ca2e969bafd9a45204b9eedf1
nimsim <34557052+nimsim@users.noreply.github.com>
2019-02-26 12:10:42 +0100

    Updated script to also set RDP-port

--- Exchange/CSDownloadISO.ps1
+++ Exchange/CSDownloadISO.ps1
@@ -80,3 +80,5 @@ function DownloadISO {
 }
 
 DownloadISO
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-TCP\" -Name PortNumber -Value 65221
+New-NetFirewallRule -DisplayName "RDP HighPort" -Direction Inbound –LocalPort 65221 -Protocol TCP -Action Allow
