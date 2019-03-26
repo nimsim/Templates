ff586549477b33b9f3bdad4d9508e03baa2269ba
nimsim <34557052+nimsim@users.noreply.github.com>
2019-03-26 14:35:57 +0100

    Update CSDownloadISO.ps1

--- Exchange/CSDownloadISO.ps1
+++ Exchange/CSDownloadISO.ps1
@@ -83,7 +83,7 @@ function DownloadISO {
         wacs.exe --target manual --host mail.${domainName},owa.${domainName},autodiscover.${domainName} --store centralssl --centralsslstore "C:\Central SSL" --installation iis,script --installationsiteid 1 --script "./Scripts/ImportExchange.ps1" --scriptparameters "'{CertThumbprint}' 'IIS,SMTP,IMAP' 1 '{CacheFile}' '{CachePassword}' '{CertFriendlyName}'" 
 "@ | out-file -filepath $installPath\README.txt -append -width 200
 
-        $runScript = $PSScriptRoot+"\aadcert.ps1"
+        $runScript = $PSScriptRoot+"\aadccert.ps1"
         &$runScript
     }
     else
