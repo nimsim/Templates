383f6d85564ebd426a70fe9b9cfbdd7d650fb15d
nimsim <34557052+nimsim@users.noreply.github.com>
2019-04-03 11:07:36 +0200

    Update aadccert.ps1

--- Exchange/aadccert.ps1
+++ Exchange/aadccert.ps1
@@ -22,10 +22,19 @@ Expand-Archive $outpath -DestinationPath $path
 
 Remove-Item -Path $outpath
 
-"`nDOWNLOAD SUCCESSFUL!`n 
+$txtOutput = @"
+DOWNLOAD SUCCESSFUL!
+
 Please follow the guide on OneNote to continue:
 https://microsoft.sharepoint.com/teams/OfficePeople/_layouts/OneNote.aspx?id=%2Fteams%2FOfficePeople%2FSiteAssets%2FOffice%20People%20Notebook&wd=target%28Projects%2FTest%20Lab.one%7CA70B945C-4284-48F6-983A-30A0C55E78C1%2FExchange%202016%20Setup%20%2B%20Hybrid%20%26%20AADC%7C97D18BAD-BFC4-4C9E-92C5-AF81E1A13042%2F%29 `
-The Configuration/Installation order should be: `n1. Set up Azure AD Connect `
-2. Set up Exchange Certificates `n3. Set up Exchange Hybrid" | out-file -filepath $installPath\README.txt -append -width 200
+
+The Configuration/Installation order should be: 
+1. Change DNS
+2. Set up Azure AD Connect
+3. Set up Exchange Certificates
+4. Set up Exchange Hybrid
+"@
+
+$txtOutput | out-file -filepath $installPath\README.txt -append -width 200
 
 Unblock-File -Path $path\Scripts\ImportExchange.ps1
