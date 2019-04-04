b4ac2b05a9068c84298cddd0a88a899324a2d2b6
nimsim <34557052+nimsim@users.noreply.github.com>
2019-04-04 15:22:48 +0200

    Update CSDownloadISO.ps1

--- Exchange/CSDownloadISO.ps1
+++ Exchange/CSDownloadISO.ps1
@@ -2,12 +2,16 @@ param (
 	[Parameter(Mandatory)]
 	[string]$domainname
 )
-        $txtOutput = @"
+
+$installPath = "$env:PUBLIC\Desktop\Hybrid"
+New-Item -Path $installPath -ItemType Directory -Force 
+
+$txtOutput = @"
 Run this from command line for Certificates to be established on your server:
 
 wacs.exe --target manual --host mail.${domainName},owa.${domainName},autodiscover.${domainName} --store centralssl --centralsslstore "C:\Central SSL" --installation iis,script --installationsiteid 1 --script "./Scripts/ImportExchange.ps1" --scriptparameters "'{CertThumbprint}' 'IIS,SMTP,IMAP' 1 '{CacheFile}' '{CachePassword}' '{CertFriendlyName}'" 
 "@ | out-file -filepath $installPath\README.txt -append -width 200
 
-        $runScript = $PSScriptRoot+"\aadccert.ps1"
-        &$runScript
-    }
+$runScript = $PSScriptRoot+"\aadccert.ps1"
+&$runScript
+    
