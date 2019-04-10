82ee5c641669944b0d493a2168c9ae66e4f41704
nimsim <34557052+nimsim@users.noreply.github.com>
2019-04-10 14:00:18 +0200

    Create startScript.ps1

---
+++ Exchange/startScript.ps1
@@ -0,0 +1,24 @@
+Param(
+    [Parameter(Mandatory=$true)]
+		[String]$domainName,
+
+		[Parameter(Mandatory=$true)]
+		[String]$VMAdminCreds
+)
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://$exchangeDomainName/PowerShell/" -Authentication Kerberos -Credential $VMAdminCreds
+Import-PSSession $Session
+New-SendConnector -Name "To internet" -AddressSpaces * -Internet
+Remove-PSSession $Session
+
+$installPath = "$env:PUBLIC\Desktop\Hybrid"
+New-Item -Path $installPath -ItemType Directory -Force 
+
+$txtOutput = @"
+Run this from command line for Certificates to be established on your server:
+
+wacs.exe --target manual --host mail.${domainName},owa.${domainName},autodiscover.${domainName} --store centralssl --centralsslstore "C:\Central SSL" --installation iis,script --installationsiteid 1 --script "./Scripts/ImportExchange.ps1" --scriptparameters "'{CertThumbprint}' 'IIS,SMTP,IMAP' 1 '{CacheFile}' '{CachePassword}' '{CertFriendlyName}'" 
+"@ | out-file -filepath $installPath\README.txt -append -width 200
+
+$runScript = $PSScriptRoot+"\aadccert.ps1"
+&$runScript
+    
