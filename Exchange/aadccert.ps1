656e3c0ea89b7b7d22b16603fe51951e1d1cf6d9
nimsim <34557052+nimsim@users.noreply.github.com>
2019-05-08 14:03:51 +0200

    Update aadccert.ps1

--- Exchange/aadccert.ps1
+++ Exchange/aadccert.ps1
@@ -35,4 +35,7 @@ The Configuration/Installation order should be:
 
 $txtOutput | out-file -filepath $installPath\README.txt -append -width 200
 
+# disable IE Enhanced Security Configuration for administrators
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -ErrorAction Stop
+
 Unblock-File -Path $path\Scripts\ImportExchange.ps1
